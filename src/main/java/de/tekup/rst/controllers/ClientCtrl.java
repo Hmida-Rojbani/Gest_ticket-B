7f10e83e9c76b7ddfe0005a83e15bf71ad8043e4
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-11-09 10:14:28 +0100

    TODO post client

---
+++ src/main/java/de/tekup/rst/controllers/ClientCtrl.java
@@ -0,0 +1,11 @@
+package de.tekup.rst.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class ClientCtrl {
+
+}
---
+++ src/main/java/de/tekup/rst/services/ClientService.java
@@ -0,0 +1,8 @@
+package de.tekup.rst.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+
+}
