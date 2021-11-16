2cea1d208864725b4089bd412c91f8542dbde430
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-11-16 11:56:08 +0100

    add validation

--- src/main/java/de/tekup/rst/dto/models/ClientReqDTO.java
+++ src/main/java/de/tekup/rst/dto/models/ClientReqDTO.java
@@ -1,19 +1,36 @@
 package de.tekup.rst.dto.models;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
 @Data
 public class ClientReqDTO {
 	
+	@NotBlank(message = "Nom est vide ")
+	@Size(min = 3, max = 50)
 	private String nom;
-	
+	@NotBlank(message = "Prenom est vide ")
+	@Size(min = 3, max = 50)
 	private String prenom;
 	
+	@Past
 	private LocalDate dateDeNaissance;
+	@Email
 	private String courriel;
 	
 	private String telephone;
+	
+	
+	/*
+	 * public void setDateDeNaissance(String date) { dateDeNaissance=
+	 * LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy")); }
+	 */
 
 }
